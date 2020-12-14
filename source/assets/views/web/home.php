757d5cd3ebad8149a9c8bf2f480942ecf9f047fa
Anderson Job Loeffler <aloeffler.j@gmail.com>
2020-12-14 05:03:02 -0300

    front página home ok

--- source/assets/views/web/home.php
+++ source/assets/views/web/home.php
@@ -1,7 +1,31 @@
 <?php $v->layout("_bootstrap") ?>
 
-<h2>Bem vindo</h2>
+<div id = "corpo" class="jumbotron vertical-center">
+    <div class="row" style="margin-bottom:50px;">
+        <h2 id="h2" class="container-fluid align-middle col-md-4 display-4 text-center">Bem Vindo</h2>
+    </div>
+    <div class="row">
+        <div id = "login" class="container-fluid border-rounded align-middle col-md-4" action="<?= $router->route("auth.userLogin"); ?>" method="post">
+            <div>
+            <a style="text-decoration:none"  href="<?= $router->route("web.user") ?>"><button class="btn btn-outline-primary btn-lg btn-block">Login Usuário do Sistema</button></a>
+            </div>
+            <br>
+            <div>
+            <a style="text-decoration:none" href="<?= $router->route("web.profissional") ?> ?>"><button class="btn btn-outline-primary btn-lg btn-block">Login Médicos</button></a>
+            </div>
+        </div>
+    </div>
+</div>
 
-<a href="<?= $router->route("web.user") ?>">Login Usuário do Sistema</a>
-<br>
-<a href="<?= $router->route("web.profissional") ?>">Login Médicos</a>
+<!--CSS--------------------------------------------------------->
+<?php $v->start("css"); ?>
+<style>
+    .vertical-center {
+    min-height: 100%;  
+    min-height: 100vh; 
+
+    display: flex;
+    align-items: center;
+    }    
+</style>
+<?php $v->end(); ?>
